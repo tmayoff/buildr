088a49611bfc15171022c0375abf005ffb60acb2
Tyler Mayoff <tyler@tylermayoff.com>
2025-05-22 10:54:24 -0400

    fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
+set -x
 
 echo "Bootstrap the build system"
 
-clang++ src/main.cpp -g -lboost_program_options $(pkg-config --cflags-only-I tomlplusplus) --std=c++23 -o buildr-bootstrap
+clang++ -g --std=c++23 -o buildr-bootstrap \
+  $(pkg-config --cflags tomlplusplus) \
+  src/main.cpp \
+  -lboost_program_options
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
+#include <boost/asio/io_context.hpp>
+#include <boost/process.hpp>
 #include <boost/program_options.hpp>
 #include <filesystem>
 #include <format>
 #include <iostream>
 
 #define TOML_EXCEPTIONS 0
+#define TOML_IMPLEMENTATION
 #include <toml++/toml.hpp>
 
 struct BuildOptions {
@@ -52,6 +55,30 @@ BuildOptions read_config() {
   return opts;
 }
 
+auto run(std::string const &cmd) {}
+
+auto get_dep_options(std::vector<std::string> deps) {
+  std::string opts;
+
+  boost::asio::io_context ctx;
+
+  for (const auto &dep : deps) {
+    boost::process::ipstream stream;
+    boost::process::child proc(
+        std::format("pkg-config --cflags --libs {}", dep),
+        boost::process::std_out > stream);
+
+    std::string line;
+    while (stream && getline(stream, line)) {
+      opts = std::format(" {}", line);
+    }
+
+    proc.wait();
+  }
+
+  return opts;
+}
+
 void build(BuildOptions const &opts) {
   namespace fs = std::filesystem;
   const fs::path build_dir = "build";
@@ -61,12 +88,13 @@ void build(BuildOptions const &opts) {
   const std::string compiler = "clang++";
   std::vector<std::filesystem::path> sources = {"src/main.cpp"};
 
+  const auto deps = get_dep_options(opts.dependencies);
+
   for (const auto &src : sources) {
-    auto const cmd = std::format(
-        "{} {} -o build/buildr {} -g "
-        "-I/nix/store/pvj7zs62mbfz6z77yq7f3896zj2qjafq-tomlplusplus-3.4.0/"
-        "include -lboost_program_options",
-        compiler, src.string(), "-std=c++23");
+    auto const cmd =
+        std::format("{} {} -DTOML_EXCEPTIONS=0 -o build/buildr {} {} -g "
+                    "-lboost_program_options",
+                    compiler, src.string(), "-std=c++23", deps);
     std::cout << "compiling: " << cmd << std::endl;
     system(cmd.c_str());
   }
